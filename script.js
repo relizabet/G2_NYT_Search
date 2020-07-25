c71aa10d2f53f734366d549f4caa7ac0dab1fe09
melaniede <debarrosmelanie@gmail.com>
2020-07-25 13:30:05 -0400

    start js file

---
+++ script.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+
+const searchTerm = $("#searchTerm");
+const numberOfRecords = $("#numberOfRecords");
+const startYear = $("#startYear");
+const endYear = $("#endYear");
+const apiKey = "UINZbaJuKzgZTYcGxYBaxsEsuQFsWB9S";
+
+let siteURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+
+$(".search").on("click", function() {
+
+queryURL = siteURL + "q=" + searchTerm.val + 
+
+})
+
+// Create on click button for search 
+
+// Grab values from input
+
+// Search API with Get method
+
+// Retrieve search data
+
+// Append results to screen
+
+
+
+
+
+});
