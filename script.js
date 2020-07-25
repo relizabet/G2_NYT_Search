57bbf855b5711d8a4294313adee9461127808cdc
Dylan Best <best.dylan15@gmail.com>
2020-07-25 13:51:37 -0400

    made query url

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-const searchTerm = $("#searchTerm");
-const numberOfRecords = $("#numberOfRecords");
-const startYear = $("#startYear");
-const endYear = $("#endYear");
-const apiKey = "UINZbaJuKzgZTYcGxYBaxsEsuQFsWB9S";
+    const searchTerm = $("#searchTerm");
+    const numberOfRecords = $("#numberOfRecords");
+    const startYear = $("#startYear");
+    const endYear = $("#endYear");
+    const apiKey = "UINZbaJuKzgZTYcGxYBaxsEsuQFsWB9S";
 
-let siteURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+    let siteURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 
+    let startYearS = "";
+    let endYearS = "";
 
-$(".search").on("click", function() {
+    $(".search").on("click", function (event) {
+        event.preventDefault();
 
-queryURL = siteURL + "q=" + searchTerm.val + 
+        startYearS = startYearS + startYear.val() + "0101";
+        endYearS = endYearS + endYear.val() + "0101";
+        console.log(startYearS);
+        console.log(endYearS);
+
+        queryURL = `${siteURL}q=${searchTerm.val}&begin_date=${startYearS}&end_date=${endYearS}&api-key=${apiKey}`;
+       
+
+        $.ajax({
+            url: queryURL, 
+            method: "GET"
+        }).then(function(response){
+
+            console.log(response);
+        })
 
 })
 
-// Create on click button for search 
+    // Create on click button for search 
 
-// Grab values from input
+    // Grab values from input
 
-// Search API with Get method
+    // Search API with Get method
 
-// Retrieve search data
+    // Retrieve search data
 
-// Append results to screen
+    // Append results to screen
 
 
 
